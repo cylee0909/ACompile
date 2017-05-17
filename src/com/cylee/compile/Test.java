618ad1e0eb1b135ec2923daed2fcea5fcdd39788
cylee <cylee0909@qq.com>
2017-05-17 23:34:16 +0800

    init compile project

---
+++ src/com/cylee/compile/Test.java
@@ -0,0 +1,10 @@
+package com.cylee.compile;
+
+/**
+ * Created by cylee on 2017/5/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
